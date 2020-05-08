94f20a745dee902bb6a65dfc5d4f501cf5a40f31
Cristóbal <cristobal.tbecerraf@gmail.com>
2020-05-08 15:17:07 -0400

    formulario de eventos y mejoras en formularios de usuario

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,23 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        @event.user_id = current_user.id
+    
+        respond_to do |format|
+          if @event.save
+            format.html { redirect_to @event, notice: 'Evento creado exitosamente.' }
+            format.json { render :show, status: :created, location: @event }
+          else
+            format.html { render :new }
+            format.json { render json: @event.errors, status: :unprocessable_entity }
+          end
+        end
+    end
 end
