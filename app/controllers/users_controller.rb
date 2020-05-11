213b385ffb929878e25794836770e1f5c75cd712
Cristóbal <cristobal.tbecerraf@gmail.com>
2020-05-11 14:41:59 -0400

    asociación de asistencia lista

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,5 @@ class UsersController < ApplicationController
 
     def profile
     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, foreign_key: "creator_id"
+  has_many :events, foreign_key: "creator_id", dependent: :destroy
+  has_many :attendances, dependent: :destroy
+  has_many :attended_events, through: :attendances, dependent: :destroy, source: :event
 end
