5824176d175b846e5c395d995239b3ee4b2c3a5f
Cristóbal <cristobal.tbecerraf@gmail.com>
2020-05-11 23:13:37 -0400

    se agrega pagina principal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-    def create
-        a = Attendance.new(user: current_user, event: event.find(params[:event_id]))
-        a.save
-    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :attendances, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "events#index"
+  root "home#index"
 end
